e6f10cbf89d49813470012bec5b8658f2466f51d
samarth-bitsgoa <g.samarth98@gmail.com>
2016-09-09 02:16:39 +0530

    Final Update

--- assign1.py
+++ assign1.py
@@ -1,14 +1,13 @@
 a="hello"
 print "The string is " + a 
-print a[0]
+
 print a[0:1]
 print a[0:2]
 print a[0:3]
 print a[0:4]
 print a[0:5]
-print a[5:4:-1]
-print a[5:3:-1]
-print a[5:2:-1]
-print a[5:1:-1]
-print a[5:0:-1]
-print a[5::-1]
+print a[4:3:-1]
+print a[4:2:-1]
+print a[4:1:-1]
+print a[4:0:-1]
+print a[4::-1]
