1cf603f1f368174c01f71ff29bf39af193af7f87
samarth-bitsgoa <g.samarth98@gmail.com>
2016-09-09 16:56:09 +0530

    Assignment 1

---
+++ ass1.py
@@ -0,0 +1,13 @@
+a='Hello'
+print "the string is:" +a
+
+print a[0:1]
+print a[0:2]
+print a[0:3]
+print a[0:4]
+print a[0:]
+print a[4:3:-1]
+print a[4:2:-1]
+print a[4:1:-1]
+print a[4:0:-1]
+print a[4::-1]
